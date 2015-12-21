2ed476758f4743d77c3c69416c2c4c2d4b5efb79
Andreas Panayiotou <andrewpany2002@gmail.com>
2015-12-21 13:42:09 +0000

    More workd done on the gen watermark functions

--- PWMain.py
+++ PWMain.py
@@ -1,3 +1,36 @@
-hello
+from PyQt4 import QtGui, uic
+from PIL import Image, ImageQt
+import sys
 
-poopoo
+class MainGUI(QtGui.QMainWindow):
+
+    def __init__(self):
+
+        QtGui.QMainWindow.__init__(self)
+        uic.loadUi('BaseGUI.ui', self)
+        self.setLabel()
+        self.show()
+
+    def setLabel(self):
+
+        im = Image.open('Penguins.jpg')
+        im = im.convert('RGBA')
+        im = im.tobytes('raw','RGBA')
+        image = QtGui.QImage.fromData(im)
+        image = QtGui.QPixmap(image)
+        print(type(image))
+        self.picLabel.setPixmap(image)
+
+
+    def pil_Image_to_QPixmap(self, im):
+
+        im = ImageQt.ImageQt(im)
+        im = QtGui.QImage(im)
+        im = QtGui.QPixmap.fromImage(im)
+        return im
+
+if __name__ == '__main__':
+
+    app = QtGui.QApplication(sys.argv)
+    window = MainGUI()
+    sys.exit(app.exec_())
