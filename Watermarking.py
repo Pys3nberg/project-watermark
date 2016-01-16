34eb19bf6c50c30f4fc6fbd9b0701d35ffc9ca71
Andreas Panayiotou <andrewpany2002@gmail.com>
2016-01-16 20:11:42 +0000

    new and improved get_text_size function which uses polynomials. also added new function for saving  images as pdfs

--- Watermarking.py
+++ Watermarking.py
@@ -1,5 +1,7 @@
 from PIL import Image, ImageEnhance, ImageDraw, ImageFont
-import os
+from numpy import polyfit, poly1d
+import os, datetime
+
 
 def reduce_opacity(im, opacity):
 
@@ -55,6 +57,53 @@ def gen_watermark(im, inText):
     # return a composite image, a combination of the the input image and the new layer.
     return Image.alpha_composite(im.convert('RGBA'), layer)
 
+def watermark_full_beans(im, fileName, printNo = '000', top=None, cent=None, bot=None):
+
+    layer = Image.new('RGBA', im.size, (0,0,0,0))
+    draw = ImageDraw.Draw(layer)
+
+    draw.text((10,10), 'Print ' + printNo, font=ImageFont.truetype('arial.ttf', 18), fill=(225,0,0,255))
+
+
+    if top:
+        if top[2] == 0:
+            fntSize, tSize = get_fntSize_poly(im, draw, top[0], top[1])
+            fnt = ImageFont.truetype(top[1]+'.ttf', fntSize)
+        else:
+            fnt = ImageFont.truetype(top[1]+'.ttf', top[2])
+            tSize = draw.textsize(top[0], ImageFont.truetype(top[1]+'.ttf', top[2]))
+        pos = ((im.size[0] - tSize[0])/2, 50)
+        draw.text(pos, top[0],font=fnt,fill=(225,225,225,int(top[3]*255)))
+
+    if cent:
+        if cent[2] == 0:
+            fntSize, tSize = get_fntSize_poly(im, draw, cent[0], cent[1])
+            print(fntSize)
+            fnt = ImageFont.truetype(cent[1]+'.ttf', fntSize)
+        else:
+            fnt = ImageFont.truetype(cent[1]+'.ttf', cent[2])
+            tSize = draw.textsize(cent[0], ImageFont.truetype(cent[1]+'.ttf', cent[2]))
+        pos = ((im.size[0] - tSize[0])/2, (im.size[1] - tSize[1])/2)
+        draw.text(pos, cent[0],font=fnt,fill=(225,225,225,int(cent[3]*255)))
+
+    if not bot:
+        bot = [datetime.datetime.now().strftime('%d/%m/%Y'), 'arial', 50,128]
+
+    if bot[2] == 0:
+        fntSize, tSize = get_fntSize_poly(im, draw, bot[0], bot[1])
+        fnt = ImageFont.truetype(bot[1]+'.ttf', fntSize)
+    else:
+        fnt = ImageFont.truetype(bot[1]+'.ttf', bot[2])
+        tSize = draw.textsize(bot[0], ImageFont.truetype(bot[1]+'.ttf', bot[2]))
+
+
+    pos = ((im.size[0]/2 - tSize[0]/2), (im.size[1] - tSize[1])-50)
+    draw.text(pos, bot[0],font=fnt,fill=(225,225,225,bot[3]))
+
+
+    return Image.alpha_composite(im.convert('RGBA'), layer)
+
+
 def get_fntSize(im, drawObj, text, fntName, scale=1):
 
     """in this function we get the text size in pixels using the current fontsize, the widths of the text and the image
@@ -74,16 +123,48 @@ def get_fntSize(im, drawObj, text, fntName, scale=1):
     # return the final fontsize and the size of the text
     return fntSize-10, drawObj.textsize(text, ImageFont.truetype(fntName+'.ttf', fntSize-10))
 
+
+def get_fntSize_poly(im, drawObj, text, fntName, scale=1):
+
+    """
+    This function is an evolution of the get_fntSize which was proving to take some time in some cases. Instead of
+    looping for a undefined amount of time, we use 5 test point to fit a quadratic curve (relationship is roughly of
+    2nd order). Then finally we get the font size needed to fit the width of the image.
+    """
+
+    # List of test font sizes
+    fntSizeList = [12 ,25, 50, 75, 100]
+    textSizes = []
+
+    # Loop through sizes and get the text size in pixels using the string
+    for s in fntSizeList:
+        tSize = drawObj.textsize(text, ImageFont.truetype(fntName+'.ttf', s))
+        textSizes.append(tSize[0])
+
+    # calculate polynomial using the text size in  pixels as x, and font size as y
+    z = polyfit(textSizes,fntSizeList, 2)
+    f = poly1d(z)
+
+    # Calculate the font size needed to get the desired text size
+    fntSize = int(f((im.size[0] - 50)*scale))
+
+    return fntSize, drawObj.textsize(text, ImageFont.truetype(fntName+'.ttf', fntSize))
+
+
+def saveImage(im=None, savePath = None):
+    if savePath and im:
+        im.convert("RGB").save(savePath, "PDF", resolution=50.0)
+
+
 def main():
 
-    im = Image.open('Penguins.jpg')
-    mark = Image.open(os.path.join('Project Watermark spec', 'Watermark Eren Ramadan.jpg'))
-    final = gen_watermark(im, 'Andreas Panyiotou')
-    #final = watermark(im, mark, 0.4)
 
-    final.show()
-    final.save('hello.jpg')
-
+    # im = Image.open('Lighthouse.jpg')
+    # im = watermark_full_beans(im, '11', printNo = '000', top=['For Reference Only - Do Not distribute',
+    #                                                           'arial', 0, 0.5],
+    #                           cent=['Eren Ramadan', 'arial', 0, 0.5])
+    # im.show()
+    saveImage(savePath='hello.pdf')
 
 if __name__ == '__main__':
 
